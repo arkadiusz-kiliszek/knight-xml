1f4d2ac152c8f52648cf2cd38a6be4ab671c7811
Arkadiusz Kiliszek <arkadiusz.kiliszek@gmail.com>
2023-07-09 12:40:37 +0200

    dodanie scislego wiazania klasy rycerza z klasa zadania

---
+++ src/main/java/com/examples/knights/DamselRescuingKnight.java
@@ -0,0 +1,24 @@
+package com.examples.knights;
+
+import com.examples.quests.Quest;
+import com.examples.quests.RescueDamselQuest;
+import com.examples.quests.SlayDragonQuest;
+
+// przyklady zlych ustawien i zachowan
+public class DamselRescuingKnight implements Knight {
+    private Quest quest;
+//    private Ministrel ministrel;
+
+    public DamselRescuingKnight() {
+        //cz rycerz powinien wiedziec cos jaki quest wykonuje i zajmowa sie nim ?
+
+        this.quest = new SlayDragonQuest(System.out);
+        // czy rycerz powinien zajmowac sie swoim ministrelem ?
+//        this.ministrel = new Ministrel(System.out);
+    }
+
+    @Override
+    public void embarkOnQuest() {
+        quest.embark();
+    }
+}
